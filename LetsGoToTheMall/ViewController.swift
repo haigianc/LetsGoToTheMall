ed634956485bb593a36dd8b517319b857c6d68ea
Claudine Haigian <haigianc@bc.edu>
2020-12-04 12:33:41 -0500

    Initial Commit

---
+++ LetsGoToTheMall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LetsGoToTheMall
+//
+//  Created by Claudine Haigian on 12/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
